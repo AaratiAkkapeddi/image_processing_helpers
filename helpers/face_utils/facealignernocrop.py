307e3eed7754ae2a57f1891485cf98f7b901f9eb
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2022-09-06 09:21:36 -0400

    oops

--- helpers/face_utils/facealignernocrop.py
+++ helpers/face_utils/facealignernocrop.py
@@ -92,7 +92,7 @@ class FaceAlignerNoCrop:
 		b, g, r = cv2.split(output)
 		# Making list of Red, Green, Blue
 		# Channels and alpha
-		rgba = [0, 0, 0, 0]
+		rgba = [b, g, r, alpha]
 		# Using cv2.merge() to merge rgba
 		# into a coloured/multi-channeled image
 		dst = cv2.merge(rgba, 4)
